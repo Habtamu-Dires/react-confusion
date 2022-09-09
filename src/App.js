7b1b25da4cfd800d0f8a5e52013b5bd577216a9d
Habtamu-Dires <habtamuadugna.d@gmail.com>
2022-09-09 16:24:17 +0300

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
+class App extends Component {
+  render(){
+    return (
+      <div className='App'>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+export default App;
+
+
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +42,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+*/
